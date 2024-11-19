e49a09d0c2aedee43a4538a93f21a1f167104620
DavidHandelman1 <davidhandelman1@gmail.com>
2024-11-19 13:48:44 -0800

    Finished code to allow movement of all joints simultaneously using multi-threading

--- multi_threaded_rtc.py
+++ multi_threaded_rtc.py
@@ -1,6 +1,6 @@
 from interbotix_common_modules.common_robot.robot import robot_shutdown, robot_startup
 from interbotix_xs_modules.xs_robot.arm import InterbotixManipulatorXS
-from pynput import keyboard
+from pynput import keyboard # type: ignore
 import numpy as np
 import time
 import threading
